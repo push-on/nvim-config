ef06b37d42af73eefbddb99187b37b5aa14ec52f
mhimranhossain <mh.imranhossain@outlook.com>
2022-10-30 00:16:01 +0600

    commit

--- lua/user/plugins/lualine.lua
+++ lua/user/plugins/lualine.lua
@@ -1,35 +1,53 @@
-local status_ok, _ = pcall(require, "lualine")
-if status_ok then
-    require('lualine').setup {
-        options = {
-            icons_enabled = true,
-            theme = 'tokyonight',
-            component_separators = {left = '', right = ''},
-            section_separators = {left = '', right = ''},
-            disabled_filetypes = {statusline = {}, winbar = {}},
-            ignore_focus = {},
-            always_divide_middle = true,
-            globalstatus = true,
-            refresh = {statusline = 1000, tabline = 1000, winbar = 1000}
-        },
-        sections = {
-            lualine_a = {'mode'},
-            lualine_b = {'branch', 'diff', 'diagnostics'},
-            lualine_c = {"require'lsp-status'.status()"},
-            lualine_x = {'filetype'}
-            -- lualine_y = {''},
-            -- lualine_z = {''}
-            -- lualine_x = {'encoding', 'fileformat', 'filetype'},
-            -- lualine_y = {'progress'},
-            -- lualine_z = {'location'}
-        },
-        inactive_sections = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = {'filename'},
-            lualine_x = {'location'},
-            lualine_y = {},
-            lualine_z = {}
-        }
-    }
+local status_ok, lualine = pcall(require, "lualine")
+if not status_ok then return end
+
+local hide_in_width = function() return vim.fn.winwidth(0) > 80 end
+
+local diff = {
+    "diff",
+    colored = false,
+    symbols = {added = " ", modified = " ", removed = " "}, -- changes diff symbols
+    cond = hide_in_width
+}
+
+local mode = {"mode", fmt = function(str) return "-- " .. str .. " --" end}
+
+local filetype = {"filetype", icons_enabled = true, icon = nil}
+
+local branch = {"branch", icons_enabled = true, icon = ""}
+
+local location = {"location", padding = 0}
+
+local spaces = function()
+    return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
 end
+
+lualine.setup({
+    options = {
+        icons_enabled = true,
+        theme = "auto",
+        component_separators = {left = "", right = ""},
+        section_separators = {left = "", right = ""},
+        disabled_filetypes = {"alpha", "dashboard", "NvimTree", "Outline"},
+        always_divide_middle = true
+    },
+    sections = {
+        lualine_a = {branch},
+        lualine_b = {mode},
+        lualine_c = {},
+        -- lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_x = {diff, filetype},
+        lualine_y = {location},
+        lualine_z = {}
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {"filename"},
+        lualine_x = {"location"},
+        lualine_y = {},
+        lualine_z = {}
+    },
+    tabline = {},
+    extensions = {}
+})
