e51f19556c1024d81f21663dcd649b09c7746f09
mhimranhossain <mh.imranhossain@outlook.com>
2022-11-10 10:37:12 +0600

    hide desktop.ini

--- lua/user/plugins/nvim-tree.lua
+++ lua/user/plugins/nvim-tree.lua
@@ -14,7 +14,13 @@ if status_ok then
                 visible = false, -- when true, they will just be displayed differently than normal items
                 hide_dotfiles = false,
                 hide_gitignored = false,
-                hide_hidden = false -- only works on Windows for hidden files/directories
+                hide_hidden = false, -- only works on Windows for hidden files/directories
+                never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+                    -- ".DS_Store",
+                    -- "thumbs.db",
+                    "desktop.ini"
+                }
+
             }
         }
 
