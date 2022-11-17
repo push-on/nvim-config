89e81a053dee7abfab1db31b70b345d1143b6890
mhimranhossain <mh.imranhossain@outlook.com>
2022-11-17 16:16:39 +0600

    vs code

--- lua/user/core/keymaps.lua
+++ lua/user/core/keymaps.lua
@@ -2,56 +2,7 @@ local opts = {noremap = true, silent = true}
 
 local term_opts = {silent = true}
 
--- Shorten function name
 local keymap = vim.api.nvim_set_keymap
-
--- Remap space as leader key
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
--- Normal --
--- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
-
-keymap("n", "<leader>e", "<cmd>Neotree toggle<cr>", opts)
-keymap("n", "<leader>o", "<cmd>Neotree focus<cr>", opts)
-
-keymap("n", "<C-s>", "<cmd>w!<cr>", opts)
-keymap("n", "<C-q>", "<cmd>q!<cr>", opts)
-
-local status_ok, _ = pcall(require, "bufdelete")
-if status_ok then keymap("n", "<leader>c", "<cmd>Bdelete<cr>", opts) end
--- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
-
--- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
-
--- Move text up and down
-keymap("n", "<A-j>", ":m .+1<CR>==", opts)
-keymap("n", "<A-k>", ":m .-2<CR>==", opts)
-
--- Visual --
--- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
-
--- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
-keymap("v", "p", '"_dP', opts)
-
--- Visual Block --
--- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
