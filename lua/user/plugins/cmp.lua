441c23c467c31946b0c3fbea43fb9a65c9b98740
mhimranhossain <mh.imranhossain@outlook.com>
2022-11-24 18:37:34 +0600

    commit

--- lua/user/plugins/cmp.lua
+++ lua/user/plugins/cmp.lua
@@ -48,7 +48,8 @@ cmp.setup {
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
     ["<ESC>"] = cmp.mapping.abort(), -- close completion window
-    ["<CR>"] = cmp.mapping.confirm({ select = false })
+    ["<TAB>"] = cmp.mapping.confirm({ select = true }),
+    ["<CR>"] = cmp.mapping.confirm({ select = false }),
   }),
 
   sources = {
