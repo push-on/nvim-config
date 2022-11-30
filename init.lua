0b7b48ee0f476f00d18f42eb52b184e4858f8c9b
mhimranhossain <mh.imranhossain@outlook.com>
2022-11-30 14:28:19 +0600

    commit

--- init.lua
+++ init.lua
@@ -6,7 +6,9 @@ for _, source in ipairs {
     "user.core.options", "user.plugins-setup", "user.core.colorscheme",
     "user.plugins.treesitter", "user.plugins.mason", "user.plugins.nvim-tree",
     "user.plugins.bufferline", "user.plugins.lualine", "user.plugins.lsp",
-    "user.plugins.cmp", "user.plugins.autopairs", "user.core.keymaps","user.plugins.null-ls","user.plugins.indent-blankline"
+    "user.plugins.cmp", "user.plugins.autopairs", "user.core.keymaps",
+    "user.plugins.null-ls", "user.plugins.indent-blankline",
+    "user.plugins.comments"
 } do
     local status_ok, fault = pcall(require, source)
     if not status_ok then
@@ -14,7 +16,4 @@ for _, source in ipairs {
     end
 end
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "help",
-  command = "only",
-})
+vim.api.nvim_create_autocmd("FileType", {pattern = "help", command = "only"})
--- lua/user/plugins-setup.lua
+++ lua/user/plugins-setup.lua
@@ -2,12 +2,12 @@ local fn = vim.fn
 
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git", "clone", "--depth", "1",
-    "https://github.com/wbthomason/packer.nvim", install_path
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+    PACKER_BOOTSTRAP = fn.system {
+        "git", "clone", "--depth", "1",
+        "https://github.com/wbthomason/packer.nvim", install_path
+    }
+    print "Installing packer close and reopen Neovim..."
+    vim.cmd [[packadd packer.nvim]]
 end
 
 vim.cmd [[
@@ -21,84 +21,89 @@ local status_ok, packer = pcall(require, "packer")
 if not status_ok then return end
 
 packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end
-  }
+    display = {
+        open_fn = function()
+            return require("packer.util").float {border = "rounded"}
+        end
+    }
 }
 
 return packer.startup(function(use)
-  -- Speed up neovim
-  use "lewis6991/impatient.nvim"
-  -- Have packer manage itself
-  use "wbthomason/packer.nvim"
-  -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/popup.nvim"
-  -- Useful lua functions used ny lots of plugins
-  use "nvim-lua/plenary.nvim"
-  -- UI Component Library for Neovim
-  use "MunifTanjim/nui.nvim"
-  -- Better buffer close
-  use "famiu/bufdelete.nvim"
-  -- tokyonight
-  use "folke/tokyonight.nvim"
-  -- icons dependencies
-  use "kyazdani42/nvim-web-devicons"
-  -- Lua line status bar
-  use "nvim-lualine/lualine.nvim"
-  -- Bufferline
-  use "akinsho/bufferline.nvim"
-  -- Neo tree
-  use { "nvim-neo-tree/neo-tree.nvim", branch = "v2.x" }
+    -- Speed up neovim
+    use "lewis6991/impatient.nvim"
+    -- Have packer manage itself
+    use "wbthomason/packer.nvim"
+    -- An implementation of the Popup API from vim in Neovim
+    use "nvim-lua/popup.nvim"
+    -- Useful lua functions used ny lots of plugins
+    use "nvim-lua/plenary.nvim"
+    -- UI Component Library for Neovim
+    use "MunifTanjim/nui.nvim"
+    -- Better buffer close
+    use "famiu/bufdelete.nvim"
+    -- tokyonight
+    use "folke/tokyonight.nvim"
+    -- icons dependencies
+    use "kyazdani42/nvim-web-devicons"
+    -- Lua line status bar
+    use "nvim-lualine/lualine.nvim"
+    -- Bufferline
+    use "akinsho/bufferline.nvim"
+    -- Neo tree
+    use {"nvim-neo-tree/neo-tree.nvim", branch = "v2.x"}
 
-  -- which key
-  use "folke/which-key.nvim"
-  -- indent-blankline
-  use "lukas-reineke/indent-blankline.nvim"
-  -- completion plugin
-  use "hrsh7th/nvim-cmp"
-  -- buffer completions
-  use "hrsh7th/cmp-buffer"
-  -- path completions
-  use "hrsh7th/cmp-path"
-  -- cmdline completions
-  use "hrsh7th/cmp-cmdline"
+    -- which key
+    use "folke/which-key.nvim"
+    -- indent-blankline
+    use "lukas-reineke/indent-blankline.nvim"
+    -- completion plugin
+    use "hrsh7th/nvim-cmp"
+    -- buffer completions
+    use "hrsh7th/cmp-buffer"
+    -- path completions
+    use "hrsh7th/cmp-path"
+    -- cmdline completions
+    use "hrsh7th/cmp-cmdline"
 
-  -- snippet engine
-  use("L3MON4D3/LuaSnip")
-  -- for autocompletion
-  use("saadparwaiz1/cmp_luasnip")
-  -- useful snippets
-  use("rafamadriz/friendly-snippets")
-  -- in charge of managing lsp servers, linters & formatters
+    -- snippet engine
+    use("L3MON4D3/LuaSnip")
+    -- for autocompletion
+    use("saadparwaiz1/cmp_luasnip")
+    -- useful snippets
+    use("rafamadriz/friendly-snippets")
+    -- in charge of managing lsp servers, linters & formatters
 
-  use("williamboman/mason.nvim")
-  -- bridges gap b/w mason & lspconfig
-  use("williamboman/mason-lspconfig.nvim")
-  -- configuring lsp servers
-  use("neovim/nvim-lspconfig")
-  -- for autocompletion
-  use("hrsh7th/cmp-nvim-lsp")
-  -- formatting & linting
-  use("jose-elias-alvarez/null-ls.nvim")
-  -- bridges gap b/w mason & null-ls
-  use("jayp0521/mason-null-ls.nvim")
+    use("williamboman/mason.nvim")
+    -- bridges gap b/w mason & lspconfig
+    use("williamboman/mason-lspconfig.nvim")
+    -- configuring lsp servers
+    use("neovim/nvim-lspconfig")
+    -- for autocompletion
+    use("hrsh7th/cmp-nvim-lsp")
+    -- formatting & linting
+    use("jose-elias-alvarez/null-ls.nvim")
+    -- bridges gap b/w mason & null-ls
+    use("jayp0521/mason-null-ls.nvim")
 
-  -- treesitter configuration
-  use({
-    "nvim-treesitter/nvim-treesitter",
-    run = function()
-      require("nvim-treesitter.install").update({ with_sync = true })
-    end
-  })
-  -- auto closing
-  use("windwp/nvim-autopairs")
-  -- autoclose tags
-  use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" })
-  -- auto save
-  use "Pocco81/auto-save.nvim"
+    -- treesitter configuration
+    use({
+        "nvim-treesitter/nvim-treesitter",
+        run = function()
+            require("nvim-treesitter.install").update({with_sync = true})
+        end
+    })
+    -- auto closing
+    use("windwp/nvim-autopairs")
+    -- autoclose tags
+    use({"windwp/nvim-ts-autotag", after = "nvim-treesitter"})
+    -- auto save
+    use "Pocco81/auto-save.nvim"
 
+    -- Commenting
+    use {
+        'numToStr/Comment.nvim',
+        config = function() require('Comment').setup() end
+    }
 
-  if PACKER_BOOTSTRAP then require("packer").sync() end
+    if PACKER_BOOTSTRAP then require("packer").sync() end
 end)
